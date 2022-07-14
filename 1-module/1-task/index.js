70d42b29b0e7dc5db8b2f56d7dd0104050328c86
Daniil <lfybbk774@gmail.com>
2022-07-14 19:50:41 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+function factorial(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+    console.log(num);
+  }
+  return result;
 }
+factorial(10);
