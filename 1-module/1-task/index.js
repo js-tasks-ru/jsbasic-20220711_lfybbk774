30cb22274fb4140930e33649c387824d5894716d
Daniil <lfybbk774@gmail.com>
2022-07-15 12:30:15 +0300

    Add files via upload

---
+++ 1-module/1-task/index.js
@@ -0,0 +1,12 @@
+﻿function factorial(num) {
+  var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+    console.log(num);
+  }
+  return result;
+}
+factorial(10);
